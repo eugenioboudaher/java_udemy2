d97408177f6f7ab4a2d5e351980fc5db282f7201
eugenio <eugeniopython@gmail.com>
2025-06-03 20:57:54 -0300

    primeiro commit

---
+++ projetoAulasModulo01/strings.java
@@ -0,0 +1,13 @@
+package projetoAulasModulo01;
+
+public class strings {
+
+    public static void main (String[] args) {
+
+        System.out.println("Eugenio".charAt(0));
+
+        String e = "Euggenio abenoado por Deus".concat("!!!").toUpperCase();
+        System.out.println(e);
+        System.out.println(e.startsWith("E"));
+    }
+}

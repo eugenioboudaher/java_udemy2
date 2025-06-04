abef25643f04a628314526abfc3ffd5a5b64a3d1
eugenio <eugeniopython@gmail.com>
2025-06-03 21:31:20 -0300

    strings

--- projetoAulasModulo01/strings.java
+++ projetoAulasModulo01/strings.java
@@ -9,5 +9,15 @@ public class strings {
         String e = "Euggenio abenoado por Deus".concat("!!!").toUpperCase();
         System.out.println(e);
         System.out.println(e.startsWith("E"));
+        System.out.println(e.length());
+        System.out.println(e.endsWith("U"));
+
+        String nome = "Sueanne";
+        double dinheiro = 100.555550;
+        System.out.printf("Nome: => %s Frase que ama: => %s e possui R$ %.2f \n", nome, e,  dinheiro);
+        System.out.println(e.contains("Deus"));
+        System.out.println(e.indexOf("Deus"));
+        System.out.println(e.substring(1));
+       
     }
 }
